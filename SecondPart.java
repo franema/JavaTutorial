494d8cce2d26312d55ddb41dc8339326348d29ca
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-24 13:54:43 -0300

    Create SecondPart.java

---
+++ SecondPart.java
@@ -0,0 +1,50 @@
+public class SecondPart {
+    public static void main(String[] args) {
+
+        //Comparison. It's the same as any other language
+        // int x = 1;
+        // int y = 1;
+        // System.out.println(x == y);
+
+
+        // Logical Operators. Same &&, ||, etc... 
+        // int temperature = 22;
+        // boolean isWarm = temperature > 20 && temperature < 30;
+        // System.out.println(isWarm);
+    
+        
+        // If Statements. Nothing new here
+        // int temperature = 32;
+        // if(temperature > 30){
+        //     System.out.println("It's hot");
+        // } else if (temperature > 20) {
+        //     System.out.println("Beautiful day");
+        // } else {
+        //     System.out.println("Cold day");
+        // }
+
+
+        // Ternary Operator. Exactly like JavaScript
+        // int income  = 120_000;
+        // String className = income > 100_000 ? "First" : "Economy";
+
+
+        //Switch Statements
+        // String role = "moderator";
+        // switch (role) {
+        //     case "admin": 
+        //         System.out.println("admin");
+        //         break;
+
+        //     case "moderator": 
+        //         System.out.println("moderator");
+        //         break;
+            
+        //     default:
+        //         System.out.println("guest");
+        //     }
+
+        
+
+    }
+}

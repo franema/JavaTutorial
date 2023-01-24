551ad40ab9e8808c71e6e8e230a993800e5c7161
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-24 13:54:46 -0300

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt(); 
+        String result = "";
+
+        if (number % 5 == 0){
+            result = "Fizz";
+        }
+        if (number % 3 == 0){
+            result += "Buzz";
+        } 
+        if (result == "") {
+            result = Integer.toString(number);
+        }
+
+        System.out.println(result);
+    }
+}

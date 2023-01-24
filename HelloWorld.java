b2aa6520dff4185f3e813b07971d084ec04217c1
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-24 09:46:06 -0300

    Casting

--- HelloWorld.java
+++ HelloWorld.java
@@ -71,6 +71,20 @@ public class HelloWorld {
         // System.out.println(result);
        
         
-        // Casting
+        // Implicit Casting
+        // byte to > short > int> long > float > double. It only happen if data is not lost
+        // short x = 1;
+        // int y = x + 2;
+        // System.out.println(y);
+
+        // Explicit Casting
+        // double x = 1.1;
+        // int y = (int)x + 2;
+        // System.out.println(y);
+    
+        String x = "1";
+        int y = Integer.parseInt(x) + 2; // Float, Short etc..
+        System.out.println(y);
+
     }
 }

2230c7894b357acb612dab5b6d5f4fc632cc5abe
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-25 10:27:22 -0300

    Loops

--- SecondPart.java
+++ SecondPart.java
@@ -1,50 +1,82 @@
+import java.util.Scanner;
+
 public class SecondPart {
     public static void main(String[] args) {
 
-        //Comparison. It's the same as any other language
+        // Comparison. It's the same as any other language
         // int x = 1;
         // int y = 1;
         // System.out.println(x == y);
 
-
-        // Logical Operators. Same &&, ||, etc... 
+        // Logical Operators. Same &&, ||, etc...
         // int temperature = 22;
         // boolean isWarm = temperature > 20 && temperature < 30;
         // System.out.println(isWarm);
-    
-        
+
         // If Statements. Nothing new here
         // int temperature = 32;
         // if(temperature > 30){
-        //     System.out.println("It's hot");
+        // System.out.println("It's hot");
         // } else if (temperature > 20) {
-        //     System.out.println("Beautiful day");
+        // System.out.println("Beautiful day");
         // } else {
-        //     System.out.println("Cold day");
+        // System.out.println("Cold day");
+        // }
+
+        // Ternary Operator. Exactly like JavaScript
+        // int income = 120_000;
+        // String className = income > 100_000 ? "First" : "Economy";
+
+        // Switch Statements
+        // String role = "moderator";
+        // switch (role) {
+        // case "admin":
+        // System.out.println("admin");
+        // break;
+
+        // case "moderator":
+        // System.out.println("moderator");
+        // break;
+
+        // default:
+        // System.out.println("guest");
+        // }
+
+        // For Loops
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.println("Something");
         // }
 
 
-        // Ternary Operator. Exactly like JavaScript
-        // int income  = 120_000;
-        // String className = income > 100_000 ? "First" : "Economy";
+        // While loops
+        // int i = 0;
+        // while(i<5) {
+        //     System.out.println("Something " + i);
+        //     i++;
+        // }
 
-
-        //Switch Statements
-        // String role = "moderator";
-        // switch (role) {
-        //     case "admin": 
-        //         System.out.println("admin");
-        //         break;
-
-        //     case "moderator": 
-        //         System.out.println("moderator");
-        //         break;
-            
-        //     default:
-        //         System.out.println("guest");
-        //     }
-
+        // String input = "";
+        // Scanner scanner = new Scanner(System.in);
+        // while(!input.equals("quit")) {
+        //     System.out.print("Input: ");
+        //     input = scanner.next().toLowerCase();
+        // }
+    
+        //Do.. While
+        // String input = "";
+        // Scanner scanner = new Scanner(System.in);
+        // do {
+        //     System.out.print("Input: ");
+        //     input = scanner.next().toLowerCase();
+        // } while (!input.equals("quit"));
         
 
+
+        // For Each
+        // String[] fruits = {"Apple", "Mango", "Orange"};
+
+        // for (String fruit : fruits) {
+        //     System.out.println(fruit);
+        // }
     }
 }

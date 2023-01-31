a9cdaab646da2f16ecd7a442ed6ad4ceccdeb8b3
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-31 14:48:29 -0300

    Consumers

--- FunctionalProgramming/_Function.java
+++ FunctionalProgramming/_Function.java
@@ -1,6 +1,8 @@
 package FunctionalProgramming;
 
+import java.lang.reflect.Constructor;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
@@ -9,14 +11,17 @@ import java.util.function.Function;
 public class _Function {
 
     public static void main(String[] args) {
-        increment(0);
+        // increment(0);
 
-        System.out.println(IncrementByOne.apply(0));
+        // System.out.println(IncrementByOne.apply(0));
 
-        Function<Integer, Integer> Something = IncrementByOne.andThen(MultiplyByTen);
-        System.out.println(Something.apply(3));
+        // Function<Integer, Integer> Something = IncrementByOne.andThen(MultiplyByTen);
+        // System.out.println(Something.apply(3));
 
-        System.out.println(AddAndMultiply.apply(2, 2));
+        // System.out.println(AddAndMultiply.apply(2, 2));
+
+        Customer carlos = new Customer("Carlos", 45454545);
+        greetCustomer.accept(carlos);
     }
 
     // Function
@@ -31,5 +36,26 @@ public class _Function {
 
     static int increment(int num) {
         return num + 1;
+    }
+
+    // static void greetCustomer (Customer customer) {
+    // System.out.println("Hello " + customer.customerName);
+    // }
+
+    
+    //Consumer. Like a void function. Accepts one argument, returns nothing. BiConsumer taker two arguments
+
+    static Consumer<Customer> greetCustomer = customer -> System.out.println(customer.customerName + ("*".repeat(String.valueOf(customer.customerPhoneNumber).length())));;
+
+    static class Customer {
+
+        private final String customerName;
+        private final int customerPhoneNumber;
+
+        public Customer(String customerName, int customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+
     }
 }

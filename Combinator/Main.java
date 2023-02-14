93f8976e29203029f036d76dfddbf79e978f1c12
DonCosme <109149406+franema@users.noreply.github.com>
2023-02-14 15:13:01 -0300

    Combinator Pattern

--- Combinator/Main.java
+++ Combinator/Main.java
@@ -2,17 +2,31 @@ package Combinator;
 
 import java.time.LocalDate;
 
+import Combinator.CustomerRegistrationValidator.ValidationResult;
+
 public class Main {
     public static void main(String[] args) {
         
         Customer customer = new Customer(
             "Cacho",
             "cacho@cacho",
-            "+02454",
+            "02454",
             LocalDate.of(1998, 12, 25));
         
-        CustomerValidatorService validatorService = new CustomerValidatorService(); 
-        System.out.println(validatorService.isCustomerValid(customer));
+        // CustomerValidatorService validatorService = new CustomerValidatorService(); 
+        // System.out.println(validatorService.isCustomerValid(customer));
+
+        ValidationResult result = CustomerRegistrationValidator
+            .isEmailValid()
+            .and(CustomerRegistrationValidator.isPhoneValid())
+            .and(CustomerRegistrationValidator.isAdult())
+            .apply(customer);
+
+        System.out.println(result);
+
+        if(result != ValidationResult.SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
     }
 
 }
